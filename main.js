fc015a5cc4ed06b8902ef27a0e0bf044d4ade636
Viperr <lilboogie279@gmail.com>
2021-05-10 14:31:16 -0500

    Replaced basic command handler
    Replaced the basic command handler with a more advanced and organized command handler.

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = ';';
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+const command = require(`./commands/${file}`)
+
+client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('Itadori is ready to exorcise cursed spirits!');
@@ -17,9 +26,10 @@ client.on('message', message => {
     const command = args.shift().toLowerCase();
 
     if(command === 'ping'){
-        message.channel.send('please stop.')
-    } else if (command == 'github'){
-    message.channel.send('https://github.com/vxperr')
+        client.commands.get('ping').execute(message, args);
+    } 
+    else if (command == 'github'){
+        client.commands.get('github').execute(message, args);
     }
 })
 

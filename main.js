98a9b053bb45f0f3f9ca5e6c1b8a3721bb35e41e
Viperr <lilboogie279@gmail.com>
2021-05-10 02:22:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = ';';
+
+client.once('ready', () => {
+    console.log('Itadori is ready to exorcise cursed spirits!');
+
+    client.user.setActivity("movies.", {
+        type: "WATCHING",
+    });
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('please stop.')
+    } else if (command == 'github'){
+    message.channel.send('https://github.com/vxperr')
+    }
+})
+
+client.login('ODQxMTk5NDUzNTMxOTMwNjY1.YJjSCg.gCftaUn-cSjVBI39Ur8ef5rF-MA');
